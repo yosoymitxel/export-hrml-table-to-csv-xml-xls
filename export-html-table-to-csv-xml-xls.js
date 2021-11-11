e51b57b49506288810b6148d4b1d38d3fe0e54a8
Mitxel Code <44605381+yosoymitxel@users.noreply.github.com>
2021-11-11 18:02:25 -0300

    Update export-html-table-to-csv-xml-xls.js

--- export-html-table-to-csv-xml-xls.js
+++ export-html-table-to-csv-xml-xls.js
@@ -3,28 +3,35 @@ function nombreArchivo(nombre) {
 }
 
 function exportHTMLTableExcel(table) {
-    let name = nombreArchivo(table)
-    var uri =
-        'data:application/vnd.ms-excel;base64,',
-        template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-        base64 = function (s) {
-            return window.btoa(unescape(encodeURIComponent(s)))
-        },
-        format = function (s, c) {
-            return s.replace(/{(\w+)}/g, function (m, p) {
-                    return c[p];
-                }
-            )
-        }
-
-    if (!table.nodeType) table = document.getElementById(table)
-
-    var ctx    = {worksheet: name || 'Worksheet', table: table.innerHTML}
-    var a      = document.createElement('a');
-    a.href     = uri + base64(format(template, ctx))
-    a.download = name + '.xls';
-    //triggering the function
-    a.click();
+    let filename = nombreArchivo(table)
+    var downloadLink;
+    var dataType    = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML   = tableSelect.outerHTML.replace(/ /g, '%20');
+    
+    // Specify file name
+    filename = filename ? filename+'.xls':'excel_data.xls';
+    
+    // Create download link element
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
 }
 
 function exportHTMLTableCSV(idTable) {
